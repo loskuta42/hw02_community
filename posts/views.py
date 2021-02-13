da89ffa3d191b1c7c05c04d4804145a8fbf02367
Alexey <loskuta42@yandex.ru>
2021-02-13 20:41:46 +0300

    start HW2

---
+++ posts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Post
+
+
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:11]
+    return render(request, "index.html", {"posts": latest})
+# Create your views here.
