d0e16199a02b1b6cfa6b1ab638d81da4e58d5a7b
Alexey <loskuta42@yandex.ru>
2021-02-14 15:39:47 +0300

    after review_1_v1

--- posts/admin.py
+++ posts/admin.py
@@ -3,24 +3,18 @@ from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    # перечисляем поля, которые должны отображаться в админке
     list_display = ("pk", "text", "pub_date", "author")
-    # добавляем интерфейс для поиска по тексту постов
     search_fields = ("text",)
-    # добавляем возможность фильтрации по дате
     list_filter = ("pub_date",)
     empty_value_display = "-пусто-"
 
 
 class GroupAdmin(admin.ModelAdmin):
     list_display = ("pk", "title", "slug", "description")
-    # добавляем интерфейс для поиска по названию сообщества
     search_fields = ("title",)
-    # добавляем возможность фильтрации по слагу
     list_filter = ("slug",)
     empty_value_display = "-пусто-"
 
 
-# при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
